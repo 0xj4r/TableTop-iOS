6e603cd2c220586360f8077c2e2a7feb26e6a11b
trfergusASU <trfergus@asu.edu>
2015-05-06 09:31:01 -0700

    Added sharing capability - added Facebook login

---
+++ TableTop/Globals.swift
@@ -0,0 +1,19 @@
+//
+//  Globals.swift
+//  TableTop
+//
+//  Created by Tim Ferguson on 5/6/15.
+//  Copyright (c) 2015 Josh Ransom. All rights reserved.
+//
+
+import Foundation
+import FBSDKLoginKit
+import FBSDKCoreKit
+
+class Globals{
+    
+    
+    struct global{
+    static var currentToken = FBSDKAccessToken()
+    }
+}
